e75cbb9be86f1be437e09630afd7355861ccb678
Calvin445 <cai.calvin21@gmail.com>
2019-09-15 00:13:07 -0400

    add frontend2, with vue router

---
+++ frontend2/src/components/Button.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="button">{{msg}}</div>
+</template>
+
+<script>
+    export default {
+        props: {
+            msg: String,
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .button {
+        background-color: #3B8CD8;
+        border: none;
+        color: white;
+        margin: 10px 20px;
+        padding-top: 15px;
+        text-align: center;
+        vertical-align: middle;
+        text-decoration: none;
+        cursor: pointer;
+        border-radius: 30px;
+        width: 250px;
+        height: 40px;
+        font-size: 20px;
+    }
+</style>
